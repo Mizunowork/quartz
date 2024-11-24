e8d64e63b01eb42c0deb4907a8b62ae189eacdc1
bfahrenfort <bfahrenfort1@gmail.com>
2024-11-24 11:10:54 +1100

    feat(contentIndex): Per-folder RSS feeds

--- quartz/plugins/transformers/frontmatter.ts
+++ quartz/plugins/transformers/frontmatter.ts
@@ -139,6 +139,7 @@ declare module "vfile" {
         publish: boolean | string
         draft: boolean | string
         lang: string
+        noRSS: boolean
         enableToc: string
         cssclasses: string[]
         socialImage: string
