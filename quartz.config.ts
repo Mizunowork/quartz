8423f140be7c2db3adf6aaf733dcafcad731d566
adust09 <shoukitsuda@icloud.com>
2025-01-19 16:47:13 +0900

    add new content

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 zk Tokyo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
