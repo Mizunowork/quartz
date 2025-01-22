ef883350b9dbdc3427343bfa8a692261dafba0d5
ArenaDruid <95113209+ArenaDruid@users.noreply.github.com>
2025-01-22 21:28:55 +0800

    Quartz sync: Jan 22, 2025, 9:28 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,7 +25,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/ArenaDruid",
       "哔哩哔哩": "https://space.bilibili.com/37095972",
-      "本作品已获得CC BY 4.0许可": "https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1",
+      //"本作品已获得CC BY 4.0许可": "https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1",
     },
   }),
 }
