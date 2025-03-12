adc405cdc79e52f76552c3230b97926b47afa910
enneaa <shenb12@hotmail.com>
2025-03-13 01:57:41 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -2,14 +2,19 @@ import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
 // components shared across all pages
-export const sharedPageComponents: SharedLayout = {
+export const sharedPageComponents: SharedLayout = { 
   head: Component.Head(),
   header: [],
+  left: [Component.RecentNotes({
+    title: "最近笔记",
+    limit: 5,
+    showTags: true
+  })],
   afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Discord Community": "https://discord.gg/cRFFHYye7t", 
     },
   }),
 }
