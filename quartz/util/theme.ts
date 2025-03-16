e2ba7f01a81b843c7d4a306f81ab6a62e91fec9b
Felix Nie <hongtuo.nie@u.nus.edu>
2025-03-17 02:49:33 +0800

    Applied --titleFont to PageTitle

--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -143,6 +143,7 @@ ${stylesheet.join("\n\n")}
   --highlight: ${theme.colors.lightMode.highlight};
   --textHighlight: ${theme.colors.lightMode.textHighlight};
 
+  --titleFont: "${getFontSpecificationName(theme.typography.title)}", ${DEFAULT_SANS_SERIF};
   --headerFont: "${getFontSpecificationName(theme.typography.header)}", ${DEFAULT_SANS_SERIF};
   --bodyFont: "${getFontSpecificationName(theme.typography.body)}", ${DEFAULT_SANS_SERIF};
   --codeFont: "${getFontSpecificationName(theme.typography.code)}", ${DEFAULT_MONO};
