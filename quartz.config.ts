4cc311fa154a1983dab964261dcd210c58b7bb2f
enneaa <shenb12@hotmail.com>
2025-03-18 21:23:06 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN", 
-    baseUrl: "enneaa.netlify.app/pages",   
+    baseUrl: "enneaa.netlify.app",    
     ignorePatterns: ["private", "templates", ".obsidian"],  
     defaultDateType: "created",
     theme: {
