e5cbbe16c7a67794ab1bdf04ba6d9e625d95110b
enneaa <shenb12@hotmail.com>
2025-03-27 11:29:05 +0800

    Update Body.tsx

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -10,11 +10,7 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
       <div id="tcomment"></div>
       <script src="https://cdn.jsdelivr.net/npm/twikoo@1.6.41/dist/twikoo.min.js"></script>
       <script>
-        twikoo.init({
-          envId: 'https://enneatwikoo.netlify.app/.netlify/functions/twikoo', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
-          el: '#tcomment', // 容器元素
-          // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
-        })
+        twikoo.init({envId: 'https://enneatwikoo.netlify.app/.netlify/functions/twikoo', el: '#tcomment', })
       </script>
     </div>
   )
