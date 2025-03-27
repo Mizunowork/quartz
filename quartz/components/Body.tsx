0935a06b2d06439cf198d094c58d134e0b902415
enneaa <shenb12@hotmail.com>
2025-03-27 13:04:06 +0800

    Update Body.tsx

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -4,16 +4,7 @@ import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return (
-    <div id="quartz-body">
-      {children}
-      <div id="tcomment"></div>
-      <script src="https://cdn.jsdelivr.net/npm/twikoo@1.6.41/dist/twikoo.min.js"></script>
-      <script>
-        twikoo.init({envId: 'https://enneatwikoo.netlify.app/.netlify/functions/twikoo', el: '#tcomment', })
-      </script>
-    </div>
-  )
+  return <div id="quartz-body">{children}</div>
 }
 
 Body.afterDOMLoaded = clipboardScript
