ceac87c08036001ab8b96a7bb49fc9596635b6d3
enneaa <shenb12@hotmail.com>
2025-04-01 08:11:32 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "是話說",  
-    pageTitleSuffix: "是話說",  
+    pageTitle: "是話說🌲",  
+    pageTitleSuffix: "-是話說",  
     enableSPA: true,
     enablePopovers: true,
     analytics: {
